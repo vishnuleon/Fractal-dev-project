986698118ecd8cebfe3e1d344efb3776036f702f
Vishnu Ramkumar <82367624+vishnuleon@users.noreply.github.com>
2024-04-21 20:45:05 +0530

    Update main.tf

--- Terraform/Modules/vpc/main.tf
+++ Terraform/Modules/vpc/main.tf
@@ -5,3 +5,18 @@ resource "aws_vpc" "main" {
    name = "main"
  }
 }
+
+
+resource "aws_subnet" "subnet" {
+ vpc_id                  = aws_vpc.main.id
+ cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+ map_public_ip_on_launch = true
+ availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+ vpc_id                  = aws_vpc.main.id
+ cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+ map_public_ip_on_launch = true
+ availability_zone       = "us-east-1b"
+}
