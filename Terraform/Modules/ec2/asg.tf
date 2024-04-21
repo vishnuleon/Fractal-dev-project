855c9e16c402d5059a51a00bce90307900b76142
Vishnu Ramkumar <82367624+vishnuleon@users.noreply.github.com>
2024-04-21 21:08:46 +0530

    Update asg.tf

--- Terraform/Modules/ec2/asg.tf
+++ Terraform/Modules/ec2/asg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "ecs_asg" {
  vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
- desired_capacity    = 1
+ desired_capacity    = 2
  max_size            = 3
  min_size            = 1
 
