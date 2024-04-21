ae067a452ec643cbca5c213a7cd034ceec2a6528
Vishnu Ramkumar <82367624+vishnuleon@users.noreply.github.com>
2024-04-22 00:55:58 +0530

    Create role.tf

---
+++ Terraform/global/role.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_role" "management-role" {
+  name               = "management-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
