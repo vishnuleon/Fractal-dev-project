21067020a3fd606cc98c91de90501233a919b4b6
Vishnu Ramkumar <82367624+vishnuleon@users.noreply.github.com>
2024-04-21 20:58:04 +0530

    Update main.tf

--- Terraform/Modules/ec2/main.tf
+++ Terraform/Modules/ec2/main.tf
@@ -1,1 +1,27 @@
+resource "aws_launch_template" "ecs_lt" {
+ name_prefix   = "ecs-template"
+ image_id      = "ami-test"
+ instance_type = "t3.micro"
+ key_name      = "ec2ecsglog"
+vpc_security_group_ids = [aws_security_group.security_group.id]
+ iam_instance_profile {
+   name = "ecsInstanceRole"
+ }
 
+block_device_mappings {
+   device_name = "/dev/xvda"
+   ebs {
+     volume_size = 30
+     volume_type = "gp2"
+   }
+ }
+
+ tag_specifications {
+   resource_type = "instance"
+   tags = {
+     Name = "ecs-instance"
+   }
+ }
+
+ user_data = filebase64("${path.module}/ecs.sh")
+}
