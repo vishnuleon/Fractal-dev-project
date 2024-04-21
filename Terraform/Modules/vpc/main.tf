98588d67a6e6e46295749f80881a19b41aa5ac40
Vishnu Ramkumar <82367624+vishnuleon@users.noreply.github.com>
2024-04-21 20:48:35 +0530

    Update main.tf

--- Terraform/Modules/vpc/main.tf
+++ Terraform/Modules/vpc/main.tf
@@ -20,3 +20,53 @@ resource "aws_subnet" "subnet2" {
  map_public_ip_on_launch = true
  availability_zone       = "us-east-1b"
 }
+
+
+
+resource "aws_internet_gateway" "internet_gateway" {
+ vpc_id = aws_vpc.main.id
+ tags = {
+   Name = "internet_gateway"
+ }
+}
+
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.internet_gateway.id
+ }
+}
+
+resource "aws_route_table_association" "subnet_route" {
+ subnet_id      = aws_subnet.subnet.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_route" {
+ subnet_id      = aws_subnet.subnet2.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "security_group" {
+ name   = "ecs-security-group"
+ vpc_id = aws_vpc.main.id
+
+ ingress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = -1
+   self        = "false"
+   cidr_blocks = ["0.0.0.0/0"]
+   description = "any"
+ }
+
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
