b77f2c899ade8cf4ca0f39bb785ecf0c428bb4e0
Vishnu Ramkumar <82367624+vishnuleon@users.noreply.github.com>
2024-04-22 00:58:58 +0530

    Update role.tf

--- Terraform/global/role.tf
+++ Terraform/global/role.tf
@@ -11,3 +11,8 @@ resource "aws_iam_role" "management-role" {
     }]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "ecr_policy_attachment" {
+  role       = aws_iam_role.management-role.name
+  policy_arn = aws_iam_policy.ecs_policy.arn
+}
